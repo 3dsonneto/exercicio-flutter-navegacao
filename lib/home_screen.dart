203f2cb098e296d9e369426606ac8d50c86ef5d7
3dsonneto <edson_neto1996@hotmail.com>
2021-05-13 16:31:02 -0300

    Created the homescreen structure

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,1 +1,28 @@
+import 'package:exercicio_navegacao/second_screen.dart';
+import 'package:exercicio_navegacao/third_screen.dart';
+import 'package:flutter/material.dart';
 
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Tela Dois"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Tela Três"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
