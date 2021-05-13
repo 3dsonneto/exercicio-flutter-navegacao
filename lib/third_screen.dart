7a5aa816922746b8a2a66cc34e7ec1521a74c64a
3dsonneto <edson_neto1996@hotmail.com>
2021-05-13 16:33:43 -0300

    Created the structure and navigation on the third screen

--- lib/third_screen.dart
+++ lib/third_screen.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/material.dart';
 
+class ThirdScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Terceira Tela"),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => ThirdScreen()));
+              },
+              child: Text("Tela Três"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Tela Quatro"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
