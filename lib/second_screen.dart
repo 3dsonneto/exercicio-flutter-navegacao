54564c9374443c9ecb5cd4014de26df56f710f9d
3dsonneto <edson_neto1996@hotmail.com>
2021-05-13 16:32:41 -0300

    Created the second screen structure

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -1,1 +1,31 @@
+import 'package:exercicio_navegacao/third_screen.dart';
+import 'package:flutter/material.dart';
 
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Segunda Tela"),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Tela Três"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Tela Quatro"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
